333d3b57ee80e19eb2fba49d97be40534e19d4b8
Aymen Berbache <aymenberbache21@gmail.com>
2022-10-02 20:26:14 +0100

    finished

--- primes.py
+++ primes.py
@@ -2,5 +2,27 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise(ValueError('test'))
     list = []
+    i = 0
+    while len(list) != number_of_primes:
+        if isPrime(i):
+            list.append(i)
+        i += 1
     return list
+
+def isPrime(num):
+    # prime numbers are greater than 1
+    if num > 1:
+        flag = True
+        # check for factors
+        for i in range(2, num):
+            if (num % i) == 0:
+                # if factor is found, set flag to False
+                flag = False
+                # break out of loop
+                break
+        return flag
+    else:
+        return False
